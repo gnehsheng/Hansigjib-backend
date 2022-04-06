2de14cd3f5cf4b93d7085605a049d93ef7deaec2
wanxuanh <wanxuan.ho@gmail.com>
2022-04-06 13:25:09 +0800

    changed tags to foodtype in mongodb seed menu

--- models/Menu.js
+++ models/Menu.js
@@ -6,7 +6,7 @@ const menuSchema = new Schema({
     name: { type: String },
     description: { type: String, },
     price: { type: Number },
-    tags: [{ type: String }],
+    foodtype: { type: String },
 });
 
 const Menu = mongoose.model("Menu", menuSchema);
